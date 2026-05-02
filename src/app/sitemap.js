949f68b2704f8c036e98130e99e10b307144520c
IryssLabs <kandaputrah@gmail.com>
2026-05-02 17:10:26 +0700

    update sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,7 +1,7 @@
 export default function sitemap() {
   return [
     {
-      url: 'https://www.kyuverse.my.id', 
+      url: 'https://www.kyuverse.my.id/', 
       changeFrequency: 'monthly',
       priority: 1,
     },
